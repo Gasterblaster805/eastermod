6adddbf1102a9b40c7310e4204d7e9b79c8b3406
Gasterblaster805 <gasterblaster805@gmail.com>
2021-03-02 17:17:24 -0600

    Update (0.1.6)

--- NPCs/VoidSlime.cs
+++ NPCs/VoidSlime.cs
@@ -44,5 +44,17 @@ namespace eastermod.NPCs
         {
             Item.NewItem(npc.getRect(), mod.ItemType("VoidSoul"));
         }
-    }
+        
+        public override void HitEffect(int hitDirection, double damage)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int dustType = Main.rand.Next(139, 143);
+                int dustIndex = Dust.NewDust(npc.position, npc.width, npc.height, dustType);
+                Dust dust = Main.dust[dustIndex];
+                dust.velocity.X = dust.velocity.X + Main.rand.Next(-50, 51) * 0.01f;
+                dust.velocity.Y = dust.velocity.Y + Main.rand.Next(-50, 51) * 0.01f;
+                dust.scale *= 1f + Main.rand.Next(-30, 31) * 0.01f;
+            }
+        }
 }
